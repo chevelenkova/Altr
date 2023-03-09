c2334439290b4d847c934f6c14dd8637b938d0e3
Anna Chevelenkova <anna.chevelenkova@gmail.com>
2023-03-09 16:43:20 -0500

    fix:delete no longer needed queries

--- src/test/resources/queries/query.sql
+++ src/test/resources/queries/query.sql
@@ -1,45 +1,22 @@
 CREATE DATABASE altr;
 USE altr;
 
-create TABLE altr(
-    employeeId INTEGER PRIMARY KEY,
-    firstname varchar(30) not null,
-    lastname varchar(30) not null,
-    jobtittle varchar(20) not null,
-    age INTEGER,
-    salary INTEGER
+create TABLE book(
+    characterId INTEGER PRIMARY KEY,
+    firstName varchar(30) not null,
+    lastName varchar(30) not null,
+    occupation varchar(20) not null,
+    age INTEGER
 );
 
-select * from altr;
+insert into book(characterId, firstname, lastname, occupation, age)
+values(001,'Sherlock','Holmes','Private Detective',60);
 
-insert into altr(employeeId, firstname, lastname, jobtittle, age, salary)
-values(001,'Sherlock','Holmes','Private Detective',60,12345);
-
-select * from book;
-
-insert into altr(employeeId, firstname, lastname, jobtittle, age, salary)
-values(2,'John','Watson','Assistant',80,6789);
-
-alter TABLE altr
-DROP COLUMN salary;
+insert into book(characterId, firstname, lastname, occupation, age)
+values(2,'John','Watson','Assistant',80);
 
 commit;
 
-ALTER TABLE altr
-    RENAME TO book;
 
-select * from book;
-select concat(firstname, ' ',  lastname) as fullName from book
-order by age desc ;
 
-ALTER TABLE book
-    RENAME COLUMN employeeId TO characterId;
 
-ALTER TABLE book
-    RENAME COLUMN jobtittle TO occupation;
-
-ALTER TABLE book
-    RENAME COLUMN firstname TO firstName;
-
-ALTER TABLE book
-    RENAME COLUMN lastname TO lastName;
